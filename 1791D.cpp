6c4ab0ff8a450161e114b2007d735f5de782e29b
Edgar González <alejgc99@gmail.com>
2023-02-04 15:33:16 -0600

    1791D solved

--- 1791D.cpp
+++ 1791D.cpp
@@ -1,41 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Editorial Solution, using frecuency vector, my solution using set isnt work
+void solve() {
+    int n, ans = 0; string s;
+    cin >> n >> s;    
+    
+    vector<int> cnt(26,0), p(26,0);
+    for(auto x: s) cnt[x - 'a']++;
+
+    for(auto x: s) {
+        --cnt[x - 'a'];
+        ++p[x - 'a'];
+        int cur = 0;
+        for(int i = 0; i < 26; ++i) {
+            cur += min(1, cnt[i]) + min(1, p[i]);
+        }
+        ans = max(ans, cur);
+    }
+
+    cout << ans << "\n";
+}
+
 int main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(0);    
 
     int t;
     cin >> t;
 
     while(t--) {
-        int n;
-        string str;
-        cin >> n >> str;
-
-        if(n == 2) {
-            cout << 2 << "\n";
-            continue;
-        }
-
-        unordered_set<char> str1;
-        unordered_set<char> str2;
-
-        str1.insert(str[0]);
-        for(int i = 1; i < n; i++)
-            str2.insert(str[i]);
-
-        int maxValue = str1.size() + str2.size();
-
-        for(int i = 0; i < (n-1); i++) {
-            int possibleValue = str1.size() + str2.size();
-            maxValue = max(maxValue, possibleValue);
-            str1.insert(str[i+1]);
-            str2.erase(str2.find(str[i+1]));
-        }
-
-        cout << maxValue << "\n";
+        solve();
     }
 
     return 0;
